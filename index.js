1c5941b0a0779156e1b347c61e78f6aaafe8967c
Lena <B3drkl@gmail.com>
2024-03-15 13:36:38 +0200

    Fix redirect

--- index.js
+++ index.js
@@ -17,21 +17,32 @@
 //   }, 10);
 // });
 
-document.addEventListener('DOMContentLoaded', function () {
-  var links = document.querySelectorAll(
-    'header a[href^="https://www.instagram.com"]'
-  );
-  links.forEach(function (link) {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
-      if (
-        link.href.includes('/ne_botan_glasses') ||
-        link.href.includes('/direct/t/100608811340939')
-      ) {
-        window.open(link.href, '_blank');
-      } else {
-        window.open(link.href, '_blank');
-      }
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('header a[href^="https://www.instagram.com"]');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var newWindow = window.open(link.href, '_blank');
+            newWindow.opener = null;
+        });
     });
-  });
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var links = document.querySelectorAll(
+//     'header a[href^="https://www.instagram.com"]'
+//   );
+//   links.forEach(function (link) {
+//     link.addEventListener('click', function (event) {
+//       event.preventDefault();
+//       if (
+//         link.href.includes('/ne_botan_glasses') ||
+//         link.href.includes('/direct/t/100608811340939')
+//       ) {
+//         window.open(link.href, '_blank');
+//       } else {
+//         window.open(link.href, '_blank');
+//       }
+//     });
+//   });
+// });
